0d74e08b0762746bb40f3a1d8b246c7f35fd600a
Lounarok <wulouis0511@gmail.com>
2020-05-27 16:34:19 +0800

    First commit

---
+++ DebugLogger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Monitor
+{
+	public class DebugLogger
+	{
+		public DebugLogger( string szPath )
+		{
+			// Move file first then set path
+			MoveTooLargeLogFileToBackup( szPath );
+			m_szPath = szPath;
+		}
+
+		public void WriteMsg( string szMsg )
+		{
+			try {
+				// Incase different thread getting same file.
+				string szTimestamp = DateTime.Now.ToString( "yyyy/MM/dd HH:mm:ss fff" );
+				string szFullMsg = szTimestamp + " " + szMsg;
+				System.IO.StreamWriter sw = new System.IO.StreamWriter( m_szPath, true );
+				sw.WriteLine( szFullMsg );
+				sw.Flush();
+				sw.Close();
+				System.Diagnostics.Debug.WriteLine( szFullMsg );
+			}
+			catch( Exception ex ) {
+				System.Diagnostics.Debug.WriteLine( "DebugLogger:" + m_szPath + "\r\n" + ex.ToString() );
+			}
+		}
+
+		public static void ShowDebugMsg( string szMsg )
+		{
+			string szTimestamp = DateTime.Now.ToString( "yyyy/MM/dd HH:mm:ss fff" );
+			string szFullMsg = szTimestamp + " " + szMsg;
+			System.Diagnostics.Debug.WriteLine( szFullMsg );
+		}
+
+		public static void MoveTooLargeLogFileToBackup( string szFilePath )
+		{
+			FileInfo file = new FileInfo( szFilePath );
+			if( file.Exists == false ) {
+				return;
+			}
+			// If file is larger than ~10mb, move it to backup
+			bool bFileTooLarge = file.Length > SZFileBackupSize;
+			if( bFileTooLarge == false ) {
+				return;
+			}
+			// Copy file as backup
+			try {
+				File.Copy( szFilePath, szFilePath + SZLogFileBackupSuffix, true );
+			}
+			catch {
+				// skip errors
+			}
+			// Remove original file
+			try {
+				File.Delete( szFilePath );
+			}
+			catch {
+				// skip errors
+			}
+		}
+
+		// ================= private ====================
+		const string SZLogFileBackupSuffix = ".bk";
+
+		// If size > 10mb, move to backup
+		const long SZFileBackupSize = (long)1E6 * 10;
+
+
+
+		string m_szPath;
+	}
+}
---
+++ ForegroundTracker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Monitor
+{
+    // get from https://stackoverflow.com/questions/8840926/asynchronously-getforegroundwindow-via-sendmessage-or-something
+    class ForegroundTracker
+    {
+        // Delegate and imports from pinvoke.net:
+
+        delegate void WinEventDelegate( IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime );
+
+        [DllImport( "user32.dll" )]
+        static extern IntPtr SetWinEventHook( uint eventMin, uint eventMax, IntPtr
+           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+           uint idThread, uint dwFlags );
+
+        [DllImport( "user32.dll" )]
+        static extern bool UnhookWinEvent( IntPtr hWinEventHook );
+
+        [DllImport( "user32.dll", CharSet = CharSet.Auto )]
+        static extern int GetWindowText( IntPtr hWnd, StringBuilder title, int size );
+
+        [DllImport( "user32.dll" )]
+        static extern IntPtr GetDesktopWindow();
+
+
+        // Constants from winuser.h
+        const uint EVENT_SYSTEM_FOREGROUND = 3;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+        static IntPtr m_DESKTOP;
+        static DebugLogger m_logger = new DebugLogger( "log.txt" );
+
+        // Need to ensure delegate is not collected while we're using it,
+        // storing it in a class field is simplest way to do this.
+        static WinEventDelegate procDelegate = new WinEventDelegate( WinEventProc );
+
+        public static void Start()
+        {
+            // Listen for foreground changes across all processes/threads on current desktop...
+            IntPtr hhook = SetWinEventHook( EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero,
+                    procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT );
+
+            m_DESKTOP = GetDesktopWindow();
+            Console.WriteLine( $"DESKTP ptr {m_DESKTOP.ToInt64().ToString( "X" )}" );
+            // MessageBox provides the necessary mesage loop that SetWinEventHook requires.
+            MessageBox.Show( "Tracking focus, close message box to exit." );
+
+            UnhookWinEvent( hhook );
+        }
+
+        static void WinEventProc( IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime )
+        {
+            StringBuilder title = new StringBuilder( 256 );
+            GetWindowText( hwnd, title, 256 );
+            //if( hwnd == m_DESKTOP ) {
+            //    title.Clear();
+            //    title.Append( "DESKTOP" );
+            //}
+            string szMsg = $"Foreground changed to {title} {hwnd.ToInt64().ToString( "X" )}";
+            m_logger.WriteMsg( szMsg );
+            Console.WriteLine( szMsg );
+        }
+    }
+}
